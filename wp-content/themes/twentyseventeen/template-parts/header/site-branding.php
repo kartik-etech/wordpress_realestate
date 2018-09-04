d7a952691bc84a4fa14f96ef7a15a704566bfec2
Mirdul Varshney <coder_shubham@outlook.com>
2018-09-04 12:37:10 +0530

    add custom code for add custom requset button pr again.

--- wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
+++ wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
@@ -30,10 +30,10 @@
 			<?php endif; ?>
 		</div><!-- .site-branding-text -->
 		<!-- add new code  to request call-->
-		<div class='request'>
+		<div class='request-button'>
 			<button>Request A Call</button>
 		</div>
-		<div class="rCall">
+		<div class="request-number">
 			<span><i class="fa fa-mobile-alt"></i>Call Us</span>
 			<h6>877.881.9523</h6>
 		</div>
